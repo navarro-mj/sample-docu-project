a4c262330dbe4578e91929fe1ad90aad4dc9142f
mnavarro <navarrov.mj@gmail.com>
2023-05-26 18:08:25 -0600

    simple example

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'navarrov-mj', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'sample-docu-project', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -65,22 +65,16 @@ const config = {
     ({
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
-      navbar: {
+      navbar: {       //NAVBAR 
         title: 'Docu Project',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
         },
         items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          },
           //{to: '/blog', label: 'Blog', position: 'left'}, //disable blog link
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/navarro-mj/sample-docu-project',
             label: 'GitHub',
             position: 'right',
           },
